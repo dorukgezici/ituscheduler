d4cc2caa97d7aab1dd7b68e2f0257bfc7b768223
Doruk Gezici <doruk@gezici.me>
2023-09-18 16:34:15 +0200

    Reload on login

--- frontend/src/components/SupabaseAuth/index.tsx
+++ frontend/src/components/SupabaseAuth/index.tsx
@@ -1,27 +1,26 @@
 import { clientComponentClient } from "@/lib/supabaseClient";
 import { Auth } from "@supabase/auth-ui-react";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
-// import { useEffect } from "react";
+import { useEffect } from "react";
 
 export default function SupabaseAuth({ site }: { site?: URL }) {
   const supabase = clientComponentClient();
 
-  // useEffect(() => {
-  //   const {
-  //     data: { subscription },
-  //   } = supabase.auth.onAuthStateChange((event, session) => {
-  //     if (event == "SIGNED_IN") location.reload();
-  //   });
+  useEffect(() => {
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((event, session) => {
+      if (event == "SIGNED_IN") location.reload();
+    });
 
-  //   return () => subscription.unsubscribe();
-  // }, []);
+    return () => subscription.unsubscribe();
+  }, []);
 
   return (
     <Auth
       supabaseClient={supabase}
       appearance={{ theme: ThemeSupa }}
       theme="dark"
-      dark
       socialLayout="horizontal"
       providers={["facebook", "twitter"]}
       onlyThirdPartyProviders
