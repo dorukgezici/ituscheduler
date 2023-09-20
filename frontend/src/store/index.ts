6d0d5abd879b8532d8377494a5a8ace2dceb2d0d
Doruk Gezici <doruk@gezici.me>
2023-09-20 14:02:39 +0200

    Load selected major for user on mount

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,15 +1,43 @@
 import { clientComponentClient } from "@/lib/supabaseClient";
-import { atom, onSet } from "nanostores";
+import { atom, onMount, onSet } from "nanostores";
 
 export const $selectedMajor = atom<string>("BLG");
+onMount($selectedMajor, () => {
+  const loadSelectedMajor = async () => {
+    const supabase = clientComponentClient();
+    const {
+      data: { session },
+    } = await supabase.auth.getSession();
+
+    const { data: major } = await supabase.from("user_major").select("major").eq("user_id", session?.user.id).single();
+    $selectedMajor.set(major?.major);
+  };
+
+  loadSelectedMajor();
+});
+onSet($selectedMajor, async ({ newValue }) => {
+  const oldValue = $selectedMajor.get();
+  if (newValue && newValue !== oldValue) {
+    const supabase = clientComponentClient();
+    const {
+      data: { session },
+    } = await supabase.auth.getSession();
+
+    await supabase
+      .from("user_major")
+      .upsert({ user_id: session?.user.id, major: newValue })
+      .eq("user_id", session?.user.id);
+  }
+});
+
 export const $selectedCourseCode = atom<string | undefined>();
 export const $selectedDay = atom<string | undefined>();
 
 export const $selectedSchedule = atom<string | undefined>();
 onSet($selectedSchedule, async ({ newValue }) => {
-  const supabase = clientComponentClient();
   const oldValue = $selectedSchedule.get();
   if (newValue !== oldValue) {
+    const supabase = clientComponentClient();
     if (oldValue) await supabase.from("schedules").update({ is_selected: false }).eq("id", oldValue);
     if (newValue) await supabase.from("schedules").update({ is_selected: true }).eq("id", newValue);
   }
