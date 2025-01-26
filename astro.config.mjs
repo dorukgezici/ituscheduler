cb7a953433781c386d98c6a06998ca873201cfa9
Doruk Gezici <doruk@gezici.me>
2025-01-26 03:06:55 +0100

    Trying out Deno

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,23 @@
-import vercel from "@astrojs/vercel";
+import { defineConfig, passthroughImageService } from "astro/config";
+import deno from "@deno/astro-adapter";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import sentry from "@sentry/astro";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://ituscheduler.com",
   output: "server",
-  adapter: vercel(),
+  adapter: deno({
+    esbuild: {
+      platform: "node",
+      external: ["fsevents", "@babel/preset-typescript/package.json"],
+    },
+  }),
+  image: {
+    service: passthroughImageService(),
+  },
   integrations: [
     react(),
     sitemap(),
